3cb2d420f29a9f1ee482ae356bee861e23b043ac
Qendolin <32160662+Qendolin@users.noreply.github.com>
2018-04-30 19:40:56 +0200

    CollisonComp Klasse hinzugefügt aber noch nichts drinnen

---
+++ core/src/com/mygdx/game/entityComponents/visualComps/CollisonComp.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.entityComponents.visualComps;
+
+import com.badlogic.ashley.core.Component;
+
+public class CollisonComp implements Component {
+	//TODO: Eine Klasse Impelmentieren die einen 2 collider von "bodx2D" enthält
+}
